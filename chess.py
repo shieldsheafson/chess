651f5f379fdefb818caaf7fd2210e7e162d1ef12
shieldsheafson <122336263+shieldsheafson@users.noreply.github.com>
2023-07-01 13:32:56 -0500

    Highlight current piece

--- chess.py
+++ chess.py
@@ -7,10 +7,17 @@ class game:
   def __init__(self, root):
     
     self.root = root
+
     self.squareSize = 75
+
     self.initialPosition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-    self.squares = []
-    self.board = [[[None, None] for y in range(8)] for x in range(8)]
+    self.currentPieceSelected = None
+    
+    # first None is the widget for that square, 
+    # second None is the piece on the square,
+    # third None is the color of that square
+    self.board = [[[None, None, None] for y in range(8)] for x in range(8)]
+    self.squares = {}
     self.images = []
 
   def createGameInstance(self):
@@ -74,7 +81,6 @@ class game:
     self.enPassantTargets = FEN[3]
     self.halfMoves = int(FEN[4])
     self.fullMoves = int(FEN[5])
-    
     updateUI(self)
     
   def convertToFEN(self):
@@ -108,24 +114,38 @@ class game:
   
 def updateUI(self):
 
+
   for y in self.board:
 
     for x in y:
       
+      x[0].delete()
+
       if x[1]:
         img = Image.open(returnPieceImagePath(self, x[1]))
         img = img.resize((self.squareSize, self.squareSize))
         img = ImageTk.PhotoImage(img)
-        self.images.append(img)
+        self.images.append(img) # prevents image from being discarded by python or tkinter
         x[0].create_image(self.squareSize/2, self.squareSize/2, image=img)
-        
+        if self.currentPieceSelected != x[0]:
+          x[0]['bg'] = x[2]
+
+def onPieceClick(self, widget):
+  
+  self.currentPieceSelected = widget
+
+  if widget.find_all():
+    widget['bg'] = 'red'
+
+  updateUI(self)
+
 def returnPieceFEN(self, pieceString: str):
 
-  return pieceString.split(',', 1)[0]
+  return pieceString.split(',', 2)[0]
 
 def returnPieceImagePath(self, pieceString: str):
 
-  return pieceString.split(',', 1)[1]
+  return pieceString.split(',', 2)[-1]
 
 def createSquare(self, isWhite: bool, x: int, y: int):
 
@@ -133,12 +153,18 @@ def createSquare(self, isWhite: bool, x: int, y: int):
     square_frames = tk.Frame(self.root, width= self.squareSize, height=self.squareSize)
     square_frames.grid(row=y, column=x+1) # x+1 to make room for notation marks
     self.board[y][x][0] = tk.Canvas(square_frames, background='light gray', width=self.squareSize, height=self.squareSize, borderwidth=0)
+    self.board[y][x][0].bind("<Button-1>", lambda e: onPieceClick(self, e.widget))
+    self.board[y][x][2] = 'light gray'
     self.board[y][x][0].grid()
+    self.squares[self.board[y][x][0]] = (x, y)
   else:
     square_frames = tk.Frame(self.root, width= self.squareSize, height=self.squareSize)
     square_frames.grid(row=y, column=x+1) # x+1 to make room for notation marks
     self.board[y][x][0] = tk.Canvas(square_frames, background='dark gray', width=self.squareSize, height=self.squareSize, borderwidth=0)
+    self.board[y][x][0].bind("<Button-1>", lambda e: onPieceClick(self, e.widget))
+    self.board[y][x][2] = 'dark gray'
     self.board[y][x][0].grid()
+    self.squares[self.board[y][x][0]] = (x, y)
 
 def king(self, color: str, initialPiecePosition: int):
 
@@ -148,7 +174,7 @@ def king(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'k'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black king.png'
-    return f'{FENInfo},{pathToImage}'
+    return f'{FENInfo},1 or 1 or None,{pathToImage}'
       
 def queen(self, color: str, initialPiecePosition: int):
 
@@ -158,7 +184,7 @@ def queen(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'q'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black queen.png'
-    return f'{FENInfo},{pathToImage}'
+    return f'{FENInfo},8 or 8 or 8,{pathToImage}'
 
 def rook(self, color: str, initialPiecePosition: int):
 
@@ -168,7 +194,7 @@ def rook(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'r'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black rook.png'
-    return f'{FENInfo},{pathToImage}'
+    return f'{FENInfo},8 or 8 or None,{pathToImage}'
     
 def bishop(self, color: str, initialPiecePosition: int):
 
@@ -178,7 +204,7 @@ def bishop(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'b'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black bishop.png'
-    return f'{FENInfo},{pathToImage}'
+    return f'{FENInfo},None or None or 8,{pathToImage}'
     
 def knight(self, color: str, initialPiecePosition: int):
 
@@ -188,7 +214,7 @@ def knight(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'n'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black knight.png'
-    return f'{FENInfo},{pathToImage}'
+    return f'{FENInfo},2 and 1 or None,{pathToImage}'
     
 def pawn(self, color: str, initialPiecePosition: int):
 
@@ -198,8 +224,8 @@ def pawn(self, color: str, initialPiecePosition: int):
     else:
       FENInfo = 'p'
       pathToImage = f'{os.path.dirname(__file__)}/pieces/black pawn.png'
-    return f'{FENInfo},{pathToImage}'
-    
+    return f'{FENInfo},1 or None or None,{pathToImage}'
+
 root = tk.Tk()
 g = game(root)
 g.createGameInstance()
